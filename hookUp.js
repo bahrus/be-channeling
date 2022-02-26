a8c9362260cb77e698bacecdb4564e5036735919
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-02-26 05:11:51 -0500

    0.0.2

--- hookUp.js
+++ hookUp.js
@@ -1,15 +1,23 @@
 export async function hookUp(target, channel) {
     let { doInit, eventFilter } = channel;
     const type = typeof eventFilter === 'string' ? eventFilter : eventFilter.type;
-    if (doInit) {
-        const { doAction } = await import('trans-render/lib/doAction.js');
-        const { getRecipientElement } = await import('trans-render/lib/getRecipientElement.js');
-        const recipientElement = await getRecipientElement(target, channel);
-        if (recipientElement !== null)
-            doAction(target, recipientElement, channel);
-    }
+    // if(doInit){
+    //     const {doAction} = await import ('trans-render/lib/doAction.js');
+    //     const {getRecipientElement} = await import ('trans-render/lib/getRecipientElement.js');
+    //     const recipientElement = await getRecipientElement(target, channel);
+    //     if(recipientElement !== null) doAction(target, recipientElement, channel);
+    // }
     const handler = async (e) => {
         const { composedPathMatch } = channel;
+        const path = e.composedPath();
+        if (e.path === undefined) {
+            /**
+             * hack alert: composedPath() disappears when doing asynchronous processing.
+            * path only supported by blink.  it doesn't disappear
+            * so add it to other browsers.
+            */
+            e.path = path;
+        }
         if (composedPathMatch !== undefined) {
             const headPath = e.composedPath()[0];
             if (headPath !== undefined && headPath.matches && !headPath.matches(composedPathMatch))
--- hookUp.ts
+++ hookUp.ts
@@ -3,15 +3,24 @@ import {IChannel} from './types';
 export async function hookUp(target: Element, channel: IChannel){
     let {doInit, eventFilter} = channel;
     const type = typeof eventFilter === 'string' ? eventFilter : eventFilter.type!;
-    if(doInit){
-        const {doAction} = await import ('trans-render/lib/doAction.js');
-        const {getRecipientElement} = await import ('trans-render/lib/getRecipientElement.js');
-        const recipientElement = await getRecipientElement(target, channel);
-        if(recipientElement !== null) doAction(target, recipientElement, channel);
-    }
+    // if(doInit){
+    //     const {doAction} = await import ('trans-render/lib/doAction.js');
+    //     const {getRecipientElement} = await import ('trans-render/lib/getRecipientElement.js');
+    //     const recipientElement = await getRecipientElement(target, channel);
+    //     if(recipientElement !== null) doAction(target, recipientElement, channel);
+    // }
 
     const handler = async (e: Event) => {
         const {composedPathMatch} = channel;
+        const path = e.composedPath();
+        if((<any>e).path === undefined){
+            /** 
+             * hack alert: composedPath() disappears when doing asynchronous processing.
+            * path only supported by blink.  it doesn't disappear
+            * so add it to other browsers.
+            */
+            (<any>e).path = path; 
+        }
         if(composedPathMatch !== undefined){
             const headPath = e.composedPath()[0] as Element;
             if(headPath !== undefined && headPath.matches && !headPath.matches(composedPathMatch)) return;
